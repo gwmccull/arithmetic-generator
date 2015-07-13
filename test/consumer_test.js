389a811508f79dfecf7a84b1d6e867f9ebf0e9eb
Garrett McCullough <garrett.mccullough@clearcapital.com>
2015-07-12 21:02:09 -0700

    fix tests

--- test/consumer_test.js
+++ test/consumer_test.js
@@ -1,40 +1,39 @@
 var chai = require('chai');
 var should = chai.should();
-var expect = chai.expect;
 var chaiHttp = require('chai-http');
-//var http = require('http');
 var server = require('../src/consumer/consumer');
-//var app = server();
-var request = require('supertest');
 
 chai.use(chaiHttp);
 
 describe('consumer', function() {
     describe('POST', function () {
-        it('should respond', function () {
+        it('should respond', function (done) {
             chai.request(server)
                 .post('/api/equation')
                 .send({ equation: '1+2=' })
-                .end(function (err, res) {
-                    res.should.have.status(400);
+                .end(function (res) {
+                    res.should.have.status(200);
+                    done();
                 });
 
         });
-        it('should respond with the correct answer', function () {
+        it('should respond with the correct answer', function (done) {
             chai.request(server)
                 .post('/api/equation')
                 .send({ equation: '1+4=' })
                 .end(function (err, res) {
                     res.body.solution.should.equal(5);
+                    done();
                 });
         });
-        it('should respond with an error', function () {
+        it('should respond with an error', function (done) {
             chai.request(server)
                 .post('/api/equation')
                 .send({ equation: '1+a=' })
                 .end(function (err, res) {
                     res.should.have.status(500);
                     res.body.error.should.equal('One of the values is NaN');
+                    done();
                 });
         });
     });
